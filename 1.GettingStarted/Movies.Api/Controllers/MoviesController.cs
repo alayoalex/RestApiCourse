f803e111b09b3c4c235b0d141871a1eb8954ab6b
53555 <53555@ALEXEI-PC>
2025-03-08 00:52:25 -0500

    Add project files.

---
+++ 1.GettingStarted/Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Model;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+using System.Reflection;
+
+namespace Movies.Api.Controllers
+{
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        [HttpGet(ApiEndpoints.Movies.GetAll)]
+        public async Task<IActionResult> GetAll()
+        {
+            var movies = await _movieRepository.GetAllAsync();
+            var moviesResponse = movies.MapToMoviesResponse();
+            return Ok(movies);
+        }
+
+        [HttpGet(ApiEndpoints.Movies.Get)]
+        public async Task<IActionResult> Get([FromRoute]Guid id)
+        {
+            var movie = await _movieRepository.GetByIdAsync(id);
+            if (movie is null)
+            {
+                return NotFound();
+            }
+
+            var response = movie.MapToMovieResponse();
+            return Ok(response);
+        }
+
+        [HttpPost(ApiEndpoints.Movies.Create)]
+        public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
+        {
+            var movie = request.MapToMovie();
+            var result = await _movieRepository.CreateAsync(movie);
+            return Created($"{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
+        }
+    }
+}
---
+++ 1.GettingStarted/Movies.Api/Program.cs
@@ -0,0 +1,30 @@
+using Movies.Application.Repositories;
+using Movies.Application;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication(); // Ensure this method is defined in the Movies.Application.Extensions namespace
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
