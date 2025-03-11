95947d40f46acf65caefc8dd6c83416f024e517a
Alexei Alayo <alexei.alayo@gmail.com>
2025-03-11 02:13:07 -0400

    Adding database config

---
+++ 1.GettingStarted/Movies.Application/Database/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Application.Database
+{
+    public class DbInitializer
+    {
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+
+        public DbInitializer(IDbConnectionFactory dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
+        public async Task InitializeAsync()
+        {
+            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+            await connection.ExecuteAsync(@"
+                CREATE TABLE IF NOT EXISTS movies (
+                    id UUID PRIMARY KEY,
+                    slug TEXT NOT NULL,
+                    title TEXT NOT NULL,
+                    yearofrelease INTEGER NOT NULL,
+                    genres TEXT[] NOT NULL
+                );
+            ");
+            
+            await connection.ExecuteAsync("""
+                create unique index concurrently if not exists movies_slug_idx on movies using btree(slug);
+                """);
+        }
+    }
+}
